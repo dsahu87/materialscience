79db692c3fcd2e5de5969c833882432595a1e902
debashis <debaacsir@gmail.com>
2025-03-26 20:29:30 +0530

    keep python

--- Changing POSCAR.R
+++
@@ -1,6 +0,0 @@
-setwd("/Users/dsahu/Desktop/")
-file <- "/Users/dsahu/Desktop/POSCAR"
-filedata <-read.table(file, skip=9, colClasses=c("character")) 
-changedata <- as.numeric(filedata[192:195,"V3"]) + 1
-filedata[192:195,"V3"]<- paste(changedata)
-write.table(filedata, file="/Users/dsahu/Desktop/POSCAR_modified")
